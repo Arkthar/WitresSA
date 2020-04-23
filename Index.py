a935186b6d6be3275188b4a9d71c49ebecce3238
Diego <diego.tapialeal@utem.cl>
2020-04-22 21:08:17 -0400

    Se crea la pagina Arkthar con formulario y falta configurar el html

--- Index.py
+++ Index.py
@@ -1,5 +1,8 @@
 #Libreria flask para pagina web
-from flask  import Flask, render_template
+from flask  import Flask, render_template,request,url_for
+from forms import DatosGrafo 
+from config import Config
+
 app = Flask(__name__)
 
 @app.route('/') #Se crea pagina principal
@@ -13,10 +16,17 @@ def about():
 @app.route('/Dostoievskys') #Se crea pagina Dostoievskys
 def Dostoievskys():
     return render_template('Dostoievskys.html')
-    
-@app.route('/Arkthar') #Se crea pagina Arkthar
+
+@app.route('/Arkthar', methods=['GET','POST']) #Se crea pagina Arkthar
 def Arkthar():
-    return render_template('Arkthar.html')
+    form = DatosGrafo(request.form)
+    if request.method == 'POST':
+        print(form.Vertices.data)
+        print(form.Aristas.data)
+        return render_template("grafo.html")
+    return render_template('Arkthar.html')    
+    
+
 
 
 if __name__=='__main__':
---
+++ forms.py
@@ -0,0 +1,8 @@
+from wtforms import Form #llama a wtforms
+from wtforms import StringField , SubmitField #tipos de datos que almacena la clase
+from wtforms.validators import DataRequired #validador
+
+class DatosGrafo(Form):#clase para los grafos
+    Vertices = StringField('Vertices', validators=[DataRequired()]) #tipo strings
+    Aristas = StringField('Aristas', validators=[DataRequired()]) #el validador acusa al usuario
+    submit = SubmitField('Ingresar')
